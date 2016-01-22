bb2c3e91597e2e513158a5ac8b8c4c0e14a2add7
Landon Conover <landon.conover@gmail.com>
2016-01-22 12:24:34 -0700

    Adding color

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const boxen = require('boxen');
 
-console.log(boxen('Landon Conover is awesome!', {padding: 1}));
+console.log(boxen('Landon Conover is awesome!', {padding: 1, margin: 1, borderColor: 'blue'}));
